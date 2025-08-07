4462f2c177a64621edb25178dd71a4d9d7b314a4
Teerapon Boonthong <t21boonthong_work@hotmail.com>
2025-08-07 10:51:40 +0700

    :tada: Begin a project.

---
+++ src/Api/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();  // ตัวช่วยเปิด metadata สำหรับ Swagger
+builder.Services.AddSwaggerGen();             // เปิดใช้งาน Swagger
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();       // เปิด endpoint /swagger/v1/swagger.json
+    app.UseSwaggerUI();     // เปิด UI /swagger/index.html
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
